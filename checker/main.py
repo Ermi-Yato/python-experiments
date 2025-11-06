2e97459457db25169474a552eb3709e4fc21ece0
Ermiyas <ermiyasbi007@gmail.com>
2025-11-06 10:53:24 +0300

    implemented move piece and movement rules

--- checker/main.py
+++ checker/main.py
@@ -7,6 +7,7 @@ CELL_SIZE = WIDTH / COLS
 BLACK = (50, 34, 19)
 WHITE = (241, 204, 164)
 
+
 # GAME BOARD
 class Board():
     def __init__(self, window, width, height):
@@ -52,8 +53,50 @@ class Board():
 
     def handle_click(self, mouseX, mouseY):
         row, col = self.onClick(mouseX,mouseY)
-        if self.piecesArray[int(row)][int(col)] != None:
-            self.selected_piece = self.piecesArray[int(row)][int(col)].color
+
+        # if no piece is selected and the clicked cell contains a piece
+        if self.selected_piece is None:
+            if self.piecesArray[int(row)][int(col)] is not None:
+                self.selected_piece = self.piecesArray[int(row)][int(col)]
+        else:
+            # the second click contains no piece
+
+            self.move_piece(self.selected_piece, row, col)
+            self.selected_piece = None
+
+    # method to move pieces
+    def move_piece(self, piece, new_row, new_col):
+        # move to another filled cell
+        if (self.piecesArray[int(new_row)][int(new_col)]) is not None:
+            print("Can't move. target already occupied.")
+            return
+        # get old row and col
+        prevRow = piece.row
+        prevCol = piece.col
+        
+        #move rules
+        if (prevRow == new_row or prevCol == new_col):
+            print("Same row movement or same col movement.")
+            return
+        if abs(new_row - prevRow) != 1 and abs(new_col - prevCol) != 1:
+            print("long jump not allowed")
+            return
+        if piece.color == "white":
+            if new_row > prevRow:
+                print("white piece backward movement...not allowed")
+                return
+        else:
+            if prevRow > new_row:
+                print("black piece backward movement...not allowed")
+                return
+
+        self.piecesArray[int(prevRow)][int(prevCol)] = None
+
+        self.piecesArray[int(new_row)][int(new_col)] = piece
+        piece.row = new_row
+        piece.col = new_col
+
+
 
 # PIECES CLASS
 class Pieces():
@@ -62,11 +105,13 @@ class Pieces():
         self.row = row
         self.col = col
         self.window = window
+        self.king = False
 
     def draw_piece(self):
         center_x = self.col * CELL_SIZE + CELL_SIZE // 2
         center_y = self.row * CELL_SIZE + CELL_SIZE // 2
         pygame.draw.circle(self.window, self.color, (center_x, center_y), 30)
+
 
 
 
