4a3f0e7700cafdd846d20b5b8c18867c5819ddb3
Ermiyas <ermiyasbi007@gmail.com>
2025-11-06 16:20:00 +0300

    further on movement rules implementation

--- checker/main.py
+++ checker/main.py
@@ -6,7 +6,8 @@ CELL_SIZE = WIDTH / COLS
 
 BLACK = (50, 34, 19)
 WHITE = (241, 204, 164)
-
+BLACK_PIECE_COLOR = (67, 71, 74)
+WHITE_PIECE_COLOR = (255,255,255)
 
 # GAME BOARD
 class Board():
@@ -24,9 +25,9 @@ class Board():
             for col in range(COLS):
                 if (row+col)%2 != 0:
                     if row < 3:
-                        self.piecesArray[row].append(Pieces((67, 71, 74), row, col, self.window))
+                        self.piecesArray[row].append(Pieces(BLACK_PIECE_COLOR, row, col, self.window))
                     elif row > 4:
-                        self.piecesArray[row].append(Pieces("white", row, col, self.window))
+                        self.piecesArray[row].append(Pieces(WHITE_PIECE_COLOR, row, col, self.window))
                     else:
                         self.piecesArray[row].append(None)
                 else:
@@ -49,50 +50,51 @@ class Board():
     def onClick(self, mouseX,mouseY):
         row = mouseY // CELL_SIZE
         col = mouseX // CELL_SIZE
-        return row, col
+        return int(row), int(col)
 
     def handle_click(self, mouseX, mouseY):
         row, col = self.onClick(mouseX,mouseY)
+        clicked_piece = self.piecesArray[row][col]
 
-        # if no piece is selected and the clicked cell contains a piece
+        # # if no piece is selected and the clicked cell contains a piece
         if self.selected_piece is None:
-            if self.piecesArray[int(row)][int(col)] is not None:
-                self.selected_piece = self.piecesArray[int(row)][int(col)]
+            if clicked_piece is not None:
+                self.selected_piece = clicked_piece
+                return
         else:
-            # the second click contains no piece
+            if clicked_piece is not None and clicked_piece.color == self.selected_piece.color:
+                self.selected_piece = clicked_piece
+                return
+            if clicked_piece is None:
+                self.move_piece(self.selected_piece, row, col)
+                self.selected_piece = None
+                return
 
-            self.move_piece(self.selected_piece, row, col)
-            self.selected_piece = None
 
     # method to move pieces
     def move_piece(self, piece, new_row, new_col):
         # move to another filled cell
-        if (self.piecesArray[int(new_row)][int(new_col)]) is not None:
+        if (self.piecesArray[new_row][new_col]) is not None:
             print("Can't move. target already occupied.")
             return
+
         # get old row and col
         prevRow = piece.row
         prevCol = piece.col
-        
+
         #move rules
-        if (prevRow == new_row or prevCol == new_col):
-            print("Same row movement or same col movement.")
-            return
-        if abs(new_row - prevRow) != 1 and abs(new_col - prevCol) != 1:
-            print("long jump not allowed")
-            return
-        if piece.color == "white":
-            if new_row > prevRow:
-                print("white piece backward movement...not allowed")
-                return
+        if (prevRow == new_row or prevCol == new_col): return
+
+        if abs(new_row - prevRow) > 1 or abs(new_col - prevCol) > 1: return
+
+        if piece.color == WHITE_PIECE_COLOR:
+            if new_row > prevRow: return
         else:
-            if prevRow > new_row:
-                print("black piece backward movement...not allowed")
-                return
+            if prevRow > new_row: return
 
-        self.piecesArray[int(prevRow)][int(prevCol)] = None
 
-        self.piecesArray[int(new_row)][int(new_col)] = piece
+        self.piecesArray[prevRow][prevCol] = None
+        self.piecesArray[new_row][new_col] = piece
         piece.row = new_row
         piece.col = new_col
 
@@ -106,6 +108,7 @@ class Pieces():
         self.col = col
         self.window = window
         self.king = False
+        self.turn = ""
 
     def draw_piece(self):
         center_x = self.col * CELL_SIZE + CELL_SIZE // 2
