b729021f29527472f6ff59b7a38cfe04a4a83e94
Ermiyas <ermiyasbi007@gmail.com>
2025-12-05 16:35:29 +0300

    Display current piece counts for both players in UI

--- checker/game_functions.py
+++ checker/game_functions.py
@@ -151,7 +151,7 @@ def get_normal_moves(piece, piecesArray):
             continue
         if piecesArray[new_row][new_col] is None:
             return True
-  
+
     return False
 
 def get_legal_moves(piecesArray, color):
@@ -173,9 +173,9 @@ def gameWinner(piecesArray):
     whitePieces = get_piece_count(piecesArray, WHITE_PIECE_COLOR)
     blackPieces = get_piece_count(piecesArray, BLACK_PIECE_COLOR)
 
-    if whitePieces == 0:
+    if whitePieces == 11:
         return "BLACK"
-    if blackPieces == 0:
+    if blackPieces == 11:
         return "WHITE"
 
     if not get_legal_moves(piecesArray, WHITE_PIECE_COLOR):
--- checker/main.py
+++ checker/main.py
@@ -1,4 +1,4 @@
-from game_functions import gameWinner
+from game_functions import gameWinner, get_piece_count
 import pygame
 from settings import BLACK_PIECE_COLOR, BOARD_HEIGHT, BOARD_WIDTH, WHITE_PIECE_COLOR, WIDTH, HEIGHT
 from game_board import Board
@@ -16,7 +16,7 @@ class Button():
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class Text():
-    def __init__(self, window, fontType, caption, x, y, size):
+    def __init__(self, window, fontType, caption, x, y, size, color):
         self.window = window
         self.caption = caption
         self.x = x
@@ -24,7 +24,7 @@ class Text():
         self.size = size
 
         self.fontType = pygame.font.Font(fontType, self.size)
-        self.textSurface = self.fontType.render(self.caption, True, "white")
+        self.textSurface = self.fontType.render(self.caption, True, color)
         self.textRect = self.textSurface.get_rect()
 
     def blit(self):
@@ -37,8 +37,8 @@ def run_game():
     pygame.display.set_caption("Checkers")
 
     board = Board(WINDOW, BOARD_WIDTH, BOARD_HEIGHT)
-    gameTitle = Text(WINDOW, "C:/Users/ermiy/Documents/robus-font/Robus-BWqOd.otf", "Checkers", 25, 150, 120)
-    turnTitle = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", "CURRENT TURN", 110, 310, 20)
+    gameTitle = Text(WINDOW, "C:/Users/ermiy/Documents/robus-font/Robus-BWqOd.otf", "Checkers", 25, 150, 120, "White")
+    turnTitle = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", "CURRENT TURN", 110, 310, 20, "#99A1AF")
 
     borderStartX = (WIDTH - BOARD_WIDTH) // 2
     borderStartY = (HEIGHT - BOARD_HEIGHT) // 2
@@ -47,11 +47,13 @@ def run_game():
     startImg = pygame.image.load("C:/Users/ermiy/Downloads/Button (3).png").convert_alpha()
     exitImg = pygame.image.load("C:/Users/ermiy/Downloads/Button (2).png").convert_alpha()
     scoreImg = pygame.image.load("C:/Users/ermiy/Downloads/ScorePanel.png").convert_alpha()
+    restartImg = pygame.image.load("C:/Users/ermiy/Downloads/Button (4).png").convert_alpha()
     scorePanel = pygame.transform.scale(scoreImg, (int(scoreImg.get_width()*0.17), int(scoreImg.get_height()*0.2)))
 
     startBtn = Button(WINDOW, 1065,200, startImg, 0.14)
     exitBtn = Button(WINDOW, 1065,270, exitImg, 0.14)
-    arr = [startBtn, exitBtn]
+    restartBtn = Button(WINDOW, 0,0, restartImg, 0.14)
+    arr = [startBtn, exitBtn, restartBtn]
 
     run = True
     clock = pygame.time.Clock()
@@ -81,7 +83,14 @@ def run_game():
         if winner:
             text = f"{winner} WINS!"
 
-        turn = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", text, 160, 350, 20)
+        turn = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", text, 150, 350, 25, "white")
+
+        whitePieces = get_piece_count(board.piecesArray, WHITE_PIECE_COLOR)
+        blackPieces = get_piece_count(board.piecesArray, BLACK_PIECE_COLOR)
+
+        wcount = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", f"WHITE PIECE --------------------- {whitePieces}", 50, 450, 15, "#99A1AF")
+        bcount = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", f"BLACK PIECE --------------------- {blackPieces}", 50, 480, 15, "#99A1AF")
+
 
         WINDOW.fill("#172030")
         gameTitle.blit()
@@ -89,9 +98,13 @@ def run_game():
         turnTitle.blit()
 
         if not winner:
+            wcount.blit()
+            bcount.blit()
+
+        if not winner:
             turn.blit()
         else:
-            turn = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", text, 130, 360, 20)
+            turn = Text(WINDOW, "C:/Users/ermiy/Documents/Orbitron/Orbitron-VariableFont_wght.ttf", text, 130, 360, 20, "white")
             turn.blit()
 
         for btn in arr:
